456326414339779127fcf9fd8c4a5f3a19e16bc9
borgueweb <67424097+borgueweb@users.noreply.github.com>
2021-03-24 13:19:08 -0600

    working liquid

--- assets/js/fixed-nav.js
+++ assets/js/fixed-nav.js
@@ -1,16 +1,21 @@
 ///////////////// fixed menu on scroll for desktop
-if ($(window).width() > 992) {
+if ($(window).width() > 0) {
+    $('.first').css('padding-top', $('#navbar_top').outerHeight() + 'px');
   $(window).scroll(function(){  
      if ($(this).scrollTop() > 40) {
         $('#navbar_top').addClass("fixed-top");
         $('#navbar_top').addClass("bg-white");
+        $('#navbar_top').addClass("shadow-sm");
+        $('#navbar_top').removeClass("position-absolute");
         // add padding top to show content behind navbar
-        $('#panel-1').css('padding-top', $('#navbar_top').outerHeight() + 'px');
+        
       }else{
         $('#navbar_top').removeClass("fixed-top");
-        $('#navbar_top').removeClass("bg-white");
+        $('#navbar_top').removeClass("bg-white")
+        $('#navbar_top').removeClass("shadow-sm");;
+        $('#navbar_top').addClass("position-absolute");
          // remove padding top from body
-        $('#panel-1').css('padding-top', '0');
+        //$('#panel-1').css('padding-top', '0');
       }   
   });
 } // end if
